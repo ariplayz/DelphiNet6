08d5052ca5d2de1d7f32b1255a195984e04274e9
Ari Cummings <ari@aricummings.com>
2025-10-21 10:23:17 -0700

    add

---
+++ DelphiNet6/Controls/ProgramWidget.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DelphiNet6.Controls;
+
+public partial class ProgramWidget : UserControl
+{
+    public ProgramWidget()
+    {
+        InitializeComponent();
+    }
+}
