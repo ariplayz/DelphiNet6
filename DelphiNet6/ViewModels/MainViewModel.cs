2a0004267d241e2b6e6c3daa47c8040c66db6e9b
Ari Cummings <ari@aricummings.com>
2025-10-23 11:09:22 -0700

    fix login screen

--- DelphiNet6/ViewModels/MainViewModel.cs
+++ DelphiNet6/ViewModels/MainViewModel.cs
@@ -1,8 +1,33 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace DelphiNet6.ViewModels;
 
-public partial class MainViewModel : ViewModelBase
+public class MainViewModel : INotifyPropertyChanged
 {
-    
+    private bool _isUserLoggedIn;
+
+    public bool IsUserLoggedIn
+    {
+        get => _isUserLoggedIn;
+        set
+        {
+            if (_isUserLoggedIn != value)
+            {
+                _isUserLoggedIn = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    #region INotifyPropertyChanged Implementation
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    #endregion
 }
--- DelphiNet6/Views/MainView.axaml.cs
+++ DelphiNet6/Views/MainView.axaml.cs
@@ -1,5 +1,7 @@
 using System;
 using Avalonia.Controls;
+using Avalonia.Layout;
+using Avalonia.VisualTree;
 using DelphiNet6.Controls;
 using DelphiNet6.Models;
 
@@ -7,14 +9,36 @@ namespace DelphiNet6.Views;
 
 public partial class MainView : UserControl
 {
+    private Panel? _loginOverlay;
+    private ScrollViewer? _mainContentScrollViewer;
+
     public MainView()
     {
-        if (User.LoginStatus == false)
-        {
-            var login_window = new Login();
-            var container = this.FindControl<ContentControl>("container");
-            container.Content = login_window;
-        }
         InitializeComponent();
+        
+        // Locate elements in the UI
+        _loginOverlay = this.FindControl<Panel>("LoginOverlay");
+        _mainContentScrollViewer = this.FindControl<ScrollViewer>("MainContentScrollViewer");
+
+        SetLoginOverlay();
+    }
+
+    private void SetLoginOverlay()
+    {
+        // If the user is logged in, hide the login overlay; otherwise, show it and disable the main content
+        UpdateUI(User.LoginStatus);
+    }
+
+    private void UpdateUI(bool isUserLoggedIn)
+    {
+        if (_loginOverlay != null)
+        {
+            _loginOverlay.IsVisible = !isUserLoggedIn; // Show login overlay if not logged in
+        }
+
+        if (_mainContentScrollViewer != null)
+        {
+            _mainContentScrollViewer.IsEnabled = isUserLoggedIn; // Enable or disable main content
+        }
     }
 }
